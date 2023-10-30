d28f5ea46f8bb34fbaf174692dabd0805a87b96b
FEhyoeun <khyoeun92@gmail.com>
2023-10-30 21:30:50 +0900

    feat: mock api 연결 및 tailwind, axios, react-query 패키지 다운

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const BASE_URL = 'https://picsum.photos/v2'
+export const GET_API = `${BASE_URL}/list?`
+
+export async function getAPICall(url: string, params: object) {
+  try {
+    const response = await axios.get(url, {
+      params
+    })
+    const { data } = response
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
