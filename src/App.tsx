33c2e564e41b183a52e4fe946f8a990b7ad07b83
FEhyoeun <khyoeun92@gmail.com>
2023-11-04 20:54:42 +0900

    style: App.tsx 린트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import './App.css'
-import { GET_API, getAPICall } from './config'
+import './App.css';
+import { GET_API, getAPICall } from './config';
 
 function App() {
-  // test
   const payload = {
+    // todo: 변경 예정
     page: 1,
-    limit: 100
-  }
+    limit: 100,
+  };
 
   return (
     <>
       <h1>REACT SHOPPING</h1>
-      { console.log(getAPICall(GET_API, payload)) }
+
+      {console.log(getAPICall(GET_API, payload))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
